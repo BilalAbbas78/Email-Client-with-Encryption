c8034890d79cd17369a35b4f5e9dba50446486ef
Syed Bilal Abbas <abbasb000@gmail.com>
2022-08-28 01:25:11 +0500

    Inbox GUI Set

--- src/FrmDashboard.java
+++ src/FrmDashboard.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -25,7 +26,7 @@ public class FrmDashboard extends JFrame {
         renderer.setHorizontalTextAlignment(SwingConstants.LEADING);
 
         tabbedPane.addTab("Inbox", null, inboxPanel(), "Inbox");
-        tabbedPane.addTab("Sent", null, inboxPanel(), "Sent");
+        tabbedPane.addTab("Sent", null, new JPanel(), "Sent");
 //        tabbedPane.addTab("Long text", UIManager.getIcon("OptionPane.warningIcon"), pnl2, "Warning tool tip");
 //        tabbedPane.addTab("This is a really long text", UIManager.getIcon("OptionPane.errorIcon"), pnl3, "Error tool tip");
 
@@ -38,9 +39,44 @@ public class FrmDashboard extends JFrame {
     JPanel inboxPanel() {
         JPanel pnlInbox = new JPanel();
         pnlInbox.setLayout(null);
-        JLabel lblInbox = new JLabel("Inbox");
-        lblInbox.setBounds(200, 30, 200, 30);
-        pnlInbox.add(lblInbox);
+
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setBounds(0, 0, 1500, 350);
+        DefaultTableModel model = new DefaultTableModel(
+                new String [] {
+                "Subject", "From", "Date",
+        }, 0);
+
+        JTable tblInbox = new JTable();
+        scrollPane.setViewportView(tblInbox);
+        tblInbox.setModel(model);
+        pnlInbox.add(scrollPane);
+
+        JLabel lblFrom = new JLabel("From:");
+        lblFrom.setBounds(10, 350, 100, 30);
+        pnlInbox.add(lblFrom);
+
+        JLabel lblDate = new JLabel("Date:");
+        lblDate.setBounds(10, 380, 100, 30);
+        pnlInbox.add(lblDate);
+
+        JLabel lblSubject = new JLabel("Subject:");
+        lblSubject.setBounds(10, 410, 100, 30);
+        pnlInbox.add(lblSubject);
+
+        JTextArea txtMessage = new JTextArea();
+        txtMessage.setLineWrap(true);
+        txtMessage.setEditable(false);
+        JScrollPane spTxtMessage = new JScrollPane(txtMessage);
+        spTxtMessage.setBounds(0, 450, 1300, 370);
+        pnlInbox.add(spTxtMessage);
+
+
+
+
+//        JLabel lblInbox = new JLabel("Inbox");
+//        lblInbox.setBounds(200, 30, 200, 30);
+//        pnlInbox.add(lblInbox);
         return pnlInbox;
     }
 
